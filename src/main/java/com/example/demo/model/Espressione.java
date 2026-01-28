92a32c6fc3337f679f8201e4e7e561b453a5eb56
ViaNik <nicola.vianello@edu.iisleviponti.it>
2026-01-28 08:55:35 +0100

    win-rate

--- src/main/java/com/example/demo/model/Espressione.java
+++ src/main/java/com/example/demo/model/Espressione.java
@@ -38,7 +38,9 @@ public class Espressione {
                     break;
                 case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
                     //TODO
-                    
+
+                    //win-rate per sicurezza
+
                 default:
                     //TODO lanciare exception
 
